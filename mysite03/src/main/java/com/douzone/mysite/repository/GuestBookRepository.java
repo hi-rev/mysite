76c71a0ec2d45f81c59b1577891dd5c79437ee0e
hi-rev <hirev05@gmail.com>
2023-02-01 15:14:25 +0900

    mybatis #finish

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestBookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestBookRepository.java
@@ -1,12 +1,7 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,54 +11,19 @@ import com.douzone.mysite.vo.GuestBookVo;
 
 @Repository
 public class GuestBookRepository {
-	
 	@Autowired
 	private SqlSession sqlSession;
-	
-	@Autowired
-	private DataSource dataSource;
-	
-	public List<GuestBookVo> findAll() {		
-		return sqlSession.selectList("guestbook.findAll");
-	}
-	
-	public void insert(GuestBookVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			String sql = "insert into guestbook values(null, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getMessage());
-			pstmt.setString(4, vo.getRegDate());
-			
-			pstmt.executeUpdate();
-			
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 	
 	public void deleteByNoAndPassword(Long no, String password) {
 		Map<String, Object> map = Map.of("no", no, "password", password);
 		sqlSession.delete("guestbook.deleteByNoAndPassword", map);
 	}
 	
+	public void insert(GuestBookVo vo) {
+		sqlSession.insert("guestbook.insert", vo);
+	}
+	
+	public List<GuestBookVo> findAll() {
+		return sqlSession.selectList("guestbook.findAll");
+	}
 }
