a86af015583c1dd4f707c75b29a7bfacc81b7add
hi-rev <hirev05@gmail.com>
2023-02-07 10:37:58 +0900

    site #begin

--- mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -1,19 +1,33 @@
 package com.douzone.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.douzone.mysite.security.Auth;
+import com.douzone.mysite.service.SiteService;
+import com.douzone.mysite.vo.SiteVo;
 
 @Auth(role="ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
+	@Autowired
+	private SiteService siteService;
+	
 	@RequestMapping("")
-	public String main() {
-		System.out.println("admin/main go");
+	public String main(Model model) {
+		SiteVo vo = siteService.getSite();
+		model.addAttribute("siteVo", vo);
 		return "admin/main";
+	}
+	
+	@RequestMapping("/main/update")
+	public String update(SiteVo vo) {
+		siteService.updateSite(vo);
+		return "redirect:/admin";
 	}
 	
 	@RequestMapping("/guestbook")
