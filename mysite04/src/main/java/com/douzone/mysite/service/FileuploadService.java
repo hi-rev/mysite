c651d2cd12c0a30887caf8272d1f748e900e7a85
hi-rev <hirev05@gmail.com>
2023-02-14 11:05:30 +0900

    java config migration #finish

--- mysite04/src/main/java/com/douzone/mysite/service/FileuploadService.java
+++ mysite04/src/main/java/com/douzone/mysite/service/FileuploadService.java
@@ -6,21 +6,28 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.douzone.mysite.exception.FileuploadServiceException;
 
 @Service
+@PropertySource("classpath:com/douzone/mysite/web/fileupload.properties")
 public class FileuploadService {
-	private static String SAVE_PATH = "/mysite-uploads";
+	
+	@Autowired
+	private Environment env;
+	
 	private static String URL_PATH = "/assets/upload-images";
 
 	public String restore(MultipartFile file) {
 		String url = null;
 
 		try {
-			File uploadDirectory = new File(SAVE_PATH);
+			File uploadDirectory = new File(env.getProperty("fileupload.uploadLocation"));
 			if(!uploadDirectory.exists()) {
 				uploadDirectory.mkdirs();
 			}
@@ -39,7 +46,7 @@ public class FileuploadService {
 			System.out.println("##########" + fileSize);
 
 			byte[] data = file.getBytes();
-			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
+			OutputStream os = new FileOutputStream(env.getProperty("fileupload.uploadLocation") + "/" + saveFilename);
 			os.write(data);
 			os.close();
 
