34d73134d9ddd635a629b35abb57cad3fbe6713a
hi-rev <hirev05@gmail.com>
2023-03-21 13:43:03 +0900

    mysite07

---
+++ mysite07/frontend/src/component/gallery/ImageListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../../assets/scss/component/gallery/ImageListItem.scss';
+
+export default function ImageListItem({no, url, comment, notifyImage}) {
+    return (
+        <li className={styles.ImageListItem}>
+            <span style={{
+                backgroundImage: `url(${url})`
+            }}/>
+            <a onClick={(event) => {
+                event.preventDefault();
+                notifyImage.delete(no)
+            }}>삭제</a>
+        </li>
+    )
+}
